7a230819313ff9e8fb59cda6f65ae65c18a05535
Ahad <your-email@example.com>
2026-05-13 14:13:25 +0500

    Changes on Application process Section

--- app/components/ApplicationProcess.jsx
+++ app/components/ApplicationProcess.jsx
@@ -44,12 +44,12 @@ export default function ApplicationProcess() {
   ];
 
   return (
-    <section className="bg-white py-20 px-4 overflow-hidden">
+    <section className="bg-[#fff] py-20 px-4 overflow-hidden">
       <div className="max-w-7xl mx-auto">
         <div className="text-center lg:mb-24 mb-15">
-          <h2 className="lg:text-[51px] md:text-5xl sm:text-4xl text-[34px] font-bold! leading-[1.1]">
+          <h2 className="lg:text-[51px] md:text-5xl sm:text-4xl text-[34px] !font-bold leading-[1.1]">
             Application{" "}
-            <span className="text-[#B81C15] font-bold!">
+            <span className="text-[#B81C15] !font-bold">
               Process
             </span>
           </h2>
@@ -59,92 +59,14 @@ export default function ApplicationProcess() {
           </p>
         </div>
         <div className="hidden lg:block relative h-[850px] max-w-6xl mx-auto">
-<<<<<<< HEAD
           {steps.map((item, index) => {
             const Icon = item.icon;
             return (
-=======
-          {steps.map((item, index) => (
-            <div
-              key={index}
-              className={`absolute left-1/2 -translate-x-1/2 ${item.top} w-full`}
-            >
-
-              <div className="absolute left-1/2 -translate-x-1/2 flex flex-col items-center z-20">
-
-                <h3
-                  className="text-[64px] font-semibold leading-none text-[#B81C15]!"
-                  style={{
-                    textShadow: "0px 0px 30px rgba(221,66,66,0.9)",
-                  }}
-                >
-                  {item.id}
-                </h3>
-
-                {index !== steps.length - 1 && (
-                  <div
-                    className="w-0.5 h-26.25 mt-4"
-                    style={{
-                      background:
-                        "linear-gradient(180deg, rgba(184, 28, 21, 0) 10.1%, #B81C15 100%)",
-                    }}
-                  />
-                )}
-              </div>
-
->>>>>>> e12d78080606544674164e248010a5d65e8129d4
               <div
                 key={index}
                 className={`absolute left-1/2 -translate-x-1/2 ${item.top} w-full`}
               >
-<<<<<<< HEAD
                 <div className="absolute left-1/2 -translate-x-1/2 flex flex-col items-center z-20">
-=======
-                <div
-                  className={`w-87.5 bg-white rounded-[14px] border border-[#E6E6E6] shadow-[0px_4px_24px_4px_#00000014] p-5 ${
-                    item.side === "left"
-                      ? "text-right"
-                      : "text-left"
-                  }`}
-                >
-
-                  <div
-                    className={`flex items-center gap-2 mb-5 ${
-                      item.side === "left"
-                        ? "justify-end"
-                        : "justify-start"
-                    }`}
-                  >
-                   <div className="w-9 h-9 rounded-[12px] bg-[#FFF2F1] flex items-center justify-center">
-                     <FiArrowUpRight className="text-[#B81C15] text-[18px]" />
-                   </div>
-
-                    <span className="text-[10px] font-semibold tracking-[1.5px] text-[#B81C15] uppercase">
-                      {item.step}
-                    </span>
-                  </div>
-
-                  <h3 className="text-xl !font-bold text-[#222] leading-none mb-3">
-                    {item.title}
-                  </h3>
-
-                  <p className="text-sm leading-[1.5] text-[#737373]">
-                    {item.desc}
-                  </p>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-
-        <div className="lg:hidden relative max-w-md mx-auto">
-          <div className="space-y-12">
-            {steps.map((item, index) => (
-              <div key={index} className="relative pl-20">
-
-                <div className="absolute left-0 top-0 flex flex-col items-center">
-
->>>>>>> e12d78080606544674164e248010a5d65e8129d4
                   <h3
                     className="text-[64px] font-semibold leading-none !text-[#B81C15]"
                     style={{
